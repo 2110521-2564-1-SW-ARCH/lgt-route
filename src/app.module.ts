3245660dd9405b6c2710cd457334a1f468e2f42a
Jennisa <jennisa200@hotmail.com>
2021-09-24 23:10:33 +0700

    fix (not success)

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,13 @@ import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DatabaseModule } from './database.module';
 import { ProductsModule } from './products/products.module';
 import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
-    ProductsModule,
+    // ProductsModule,
     ConfigModule.forRoot({isGlobal: true}),
     TypeOrmModule.forRoot({
       type: 'postgres',
@@ -16,7 +17,8 @@ import { UserModule } from './user/user.module';
       autoLoadEntities: true,
       synchronize: true
     }),
-    UserModule
+    UserModule,
+    DatabaseModule
   ],
   controllers: [AppController],
   providers: [AppService],
