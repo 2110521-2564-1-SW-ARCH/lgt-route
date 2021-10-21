c9eb0161d2466d3519a78671d6845f9be7a685f3
mintcnn <mintchnrt@gmail.com>
2021-10-21 21:47:32 +0700

    edit dto

--- src/route/dto/create-route.dto.ts
+++ src/route/dto/create-route.dto.ts
@@ -1,21 +1,27 @@
 import { IsNotEmpty, IsString, IsInt } from 'class-validator';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 
 export class CreateRoutePayloadDto {
+    @ApiProperty()
     @IsString()
     @IsNotEmpty()
     source: string
 
+    @ApiProperty()
     @IsString()
     @IsNotEmpty()
     destination: string
 
+    @ApiProperty()
     @IsInt()
     @IsNotEmpty()
     time: number
 
+    @ApiProperty()
     @IsString()
     @IsNotEmpty()
     type: string
     
+    @ApiPropertyOptional()
     additional_type?: string
 }
